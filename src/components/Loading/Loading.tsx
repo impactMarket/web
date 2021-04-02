b7c9759d382dcd6aeb49d08b73d0ac5cf96a3c53
Paulo Soares <psoares.bj@gmail.com>
2021-04-02 04:39:39 +0100

    Add loading transition

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,81 @@
+import { Icon } from '../../theme/components';
+import { colors } from '../../theme';
+import { ease, transitions } from 'styled-gen';
+import { position, size } from 'polished';
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+type LoadingProps = {
+    isActive?: boolean;
+};
+
+const spinnerSize = 5;
+
+const ripple = keyframes`
+  0% {
+    transform: scale(0);
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 0.25;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 0;
+  }
+`;
+
+const LoadingWrapper = styled.div<LoadingProps>`
+    ${position('fixed', 0)};
+    ${transitions(['opacity', 'visibility'], '0.25s', ease.inOutCubic)};
+
+    align-items: center;
+    background-color: ${colors.white};
+    display: flex;
+    justify-content: center;
+    opacity: ${({ isActive }) => (isActive ? 1 : 0)};
+    visibility: ${({ isActive }) => (isActive ? 'visible' : 'hidden')};
+    z-index: 999999;
+
+    svg {
+        position: absolute;
+    }
+`;
+
+const LoadingSpinnerChild = styled.div``;
+
+const LoadingSpinner = styled.div`
+    ${size(`${spinnerSize}rem`)};
+
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    margin: ${`${spinnerSize / 1.3}rem`};
+
+    ${LoadingSpinnerChild} {
+        ${size(`${spinnerSize / 1.3}rem`)};
+        ${position('absolute')};
+
+        align-items: center;
+        display: flex;
+        justify-content: center;
+        border-radius: 50%;
+        animation: 1s ${ripple} infinite;
+    }
+`;
+
+export const Loading = (props: LoadingProps) => {
+    const { isActive } = props;
+
+    return (
+        <LoadingWrapper isActive={isActive}>
+            <LoadingSpinner>
+                <LoadingSpinnerChild>
+                    <Icon brandPrimary icon="im" sHeight={4} />
+                </LoadingSpinnerChild>
+            </LoadingSpinner>
+        </LoadingWrapper>
+    );
+};
