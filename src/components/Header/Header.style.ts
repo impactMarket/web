1d6054ff2c06d75202cad33308bc54b297416a9f
Paulo Soares <psoares.bj@gmail.com>
2021-05-14 16:21:33 +0100

    Add mobile menu

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -1,31 +1,81 @@
 import { colors } from '../../theme';
-import { mq } from 'styled-gen';
+import { ease, mq, transitions } from 'styled-gen';
+import { position, size } from 'polished';
 import styled, { css } from 'styled-components';
 
 export const HeaderContent = styled.div`
+    ${position('fixed', 0, 0, null, 0)};
+
     align-items: center;
     display: flex;
     justify-content: space-between;
+    padding: 1rem 2rem;
+    width: 100%;
+    background-color: ${colors.white};
+    z-index: 10;
+    max-width: 1280px;
+    margin: auto;
 `;
 
 export const HeaderMenuItem = styled.div`
+    width: 50%;
+
     ${mq.tablet(css`
+        width: auto;
+
         & + & {
             margin-left: 2rem;
         }
     `)}
 `;
 
-export const HeaderMenuWrapper = styled.div`
-    display: none;
+export const HeaderMenuWrapper = styled.div<{ isActive?: boolean }>`
+    transform: ${({ isActive }) => `translateX(${isActive ? 0 : '100%'})`};
+
+    ${mq.phone(css`
+        ${position('fixed', 0)};
+        ${transitions(['transform'], 500, ease.inOutCirc)};
+
+        background-color: ${colors.white};
+        display: flex;
+        flex-direction: column;
+        padding: 9rem 2rem 2rem;
+        z-index: 1;
+    `)};
 
     ${mq.tablet(css`
         display: flex;
+        transform: translateX(0);
+    `)}
+`;
+
+export const HeaderMobileMenuButton = styled.a`
+    ${size('2.625rem')};
+
+    align-items: center;
+    background-color: ${colors.backgroundSecondary};
+    border-radius: 0.75rem;
+    color: ${colors.brandPrimary};
+    display: inline-flex;
+    justify-content: center;
+    z-index: 100;
+
+    ${mq.tablet(css`
+        display: none;
+    `)}
+`;
+
+export const HeaderMobileMenuFooter = styled.div`
+    display: block;
+    margin-top: 4rem;
+
+    ${mq.tablet(css`
+        display: none;
     `)}
 `;
 
 export const HeaderWrapper = styled.div`
     background-color: ${colors.white};
-    padding: 1.5rem 0;
+    padding-top: 4.75rem;
     width: 100%;
 `;
