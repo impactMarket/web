b24d6fb20d6b652506f21a77cd75ae26559ece82
Paulo Soares <psoares.bj@gmail.com>
2021-05-21 17:11:00 +0100

    Add initial translation file

--- data/br.ts
+++ data/br.ts
@@ -1,16 +1,18 @@
 export const br = {
     config: {
         seo: {
-            description: 'Lorem ipsum',
-            image: '/img/seo.jpg',
-            keywords: 'Lorem ipsum',
-            title: 'Impact Market - Página'
+            description:
+                'impactMarket enables any vulnerable community to implement poverty alleviation mechanisms, like Unconditional Basic Income.',
+            image: 'https://impactmarket.com/img/seo.jpg',
+            keywords:
+                'impactMarket, community, unconditional, basic income, beneficiaries, cUSD, UBI, contracts, allowance, celo, blockchain, infrastructure, system, protocol',
+            title: 'impactMarket - Decentralized Anti-Poverty System'
         }
     },
     pages: {
         homepage: {
             hero: {
-                heading: 'Teste de cabeçalho'
+                heading: 'Decentralized anti&#8209;poverty system.'
             }
         }
     }
