5fb42ec84c7d8d5e6b559b7e2bfd68718ab86a84
Paulo Soares <psoares.bj@gmail.com>
2021-07-27 23:15:21 +0100

    Add fr

--- lang-config.js
+++ lang-config.js
@@ -1,3 +1,7 @@
-const langConfig = [{ code: 'en-us', isDefault: true, label: 'English', shortCode: 'en' }, { code: 'pt-br', label: 'Português - Brasil', shortCode: 'br' }];
+const langConfig = [
+    { code: 'en-us', isDefault: true, label: 'English', shortCode: 'en' },
+    { code: 'pt-br', label: 'Português - Brasil', shortCode: 'br' },
+    { code: 'fr', label: 'French', shortCode: 'fr' }
+];
 
 module.exports = langConfig;
