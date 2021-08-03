73f1cb4bf9b521bbedf018e00c5fc496dc4b0015
Paulo Soares <psoares.bj@gmail.com>
2021-08-03 12:01:05 +0100

    Add ES

--- lang-config.js
+++ lang-config.js
@@ -1,5 +1,6 @@
 const langConfig = [
     { code: 'en-us', isDefault: true, label: 'English', shortCode: 'en' },
+    { code: 'es', label: 'Español', shortCode: 'es' },
     { code: 'pt-br', label: 'Português', shortCode: 'br' },
     { code: 'fr', label: 'Français', shortCode: 'fr' }
 ];
