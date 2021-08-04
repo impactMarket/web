9be4ce1ad7a788d9ff279d07ba563400b0e45c56
Paulo Soares <psoares.bj@gmail.com>
2021-08-04 18:08:49 +0100

    Revert "Add ES"
    This reverts commit edeecc601536d5166703a968e72c95313ea068a8.

--- lang-config.js
+++ lang-config.js
@@ -1,6 +1,5 @@
 const langConfig = [
     { code: 'en-us', isDefault: true, label: 'English', shortCode: 'en' },
-    { code: 'es', label: 'Español', shortCode: 'es' },
     { code: 'pt-br', label: 'Português', shortCode: 'br' },
     { code: 'fr', label: 'Français', shortCode: 'fr' }
 ];
