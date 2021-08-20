77f645df00f17cd8f48e180cad5c67941550220e
Paulo Soares <psoares.bj@gmail.com>
2021-08-20 09:09:45 +0100

    Fix esolidar button text

--- src/page-components/Community/MainInfo/ResumeBox.tsx
+++ src/page-components/Community/MainInfo/ResumeBox.tsx
@@ -73,7 +73,7 @@ export const ResumeBox = (props: any) => {
                         sBorderColor="brandSecondaryLight"
                         thin
                     >
-                        <Text regular>
+                        <Text body medium>
                             <String id="page.community.miscDonationButtonLabel" />
                         </Text>
                     </Button>
