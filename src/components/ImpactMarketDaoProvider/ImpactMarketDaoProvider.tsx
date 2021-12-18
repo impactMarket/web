0c76cd12c88759e139b37be39b3bd9ead2c02016
Paulo Soares <psoares.bj@gmail.com>
2021-12-18 18:27:06 +0000

    Use epoch without wallet

--- src/components/ImpactMarketDaoProvider/ImpactMarketDaoProvider.tsx
+++ src/components/ImpactMarketDaoProvider/ImpactMarketDaoProvider.tsx
@@ -2,22 +2,42 @@ import '@celo-tools/use-contractkit/lib/styles.css';
 import {
     Alfajores,
     ContractKitProvider,
+    Mainnet,
     useConnectedSigner,
     useContractKit,
     useProvider
 } from '@celo-tools/use-contractkit';
 import { ImpactMarketProvider } from '@impact-market/utils';
-import React from 'react';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import React, { useEffect, useState } from 'react';
+import config from '../../../config';
 
 type ProviderProps = {
     children?: any;
 };
 
+const network = config.isDaoMainnet ? Mainnet : Alfajores;
+
 const Wrapper = (props: ProviderProps) => {
     const { children } = props;
-    const provider = useProvider();
     const signer = useConnectedSigner();
     const { address } = useContractKit();
+    const [, setIsInDifferentNetwork] = useState(false);
+    const provider = new JsonRpcProvider(network.rpcUrl);
+    const walletProvider = useProvider();
+
+    useEffect(() => {
+        const verifyNetwork = async () => {
+            const network = await provider?.getNetwork();
+            const walletNetwork = await walletProvider?.getNetwork();
+
+            setIsInDifferentNetwork(network.chainId !== walletNetwork.chainId);
+        };
+
+        if (walletProvider) {
+            verifyNetwork();
+        }
+    }, [walletProvider, provider]);
 
     return (
         <ImpactMarketProvider address={address} provider={provider} signer={signer}>
@@ -35,7 +55,7 @@ export const ImpactMarketDaoProvider = ({ children }: ProviderProps) => {
                 name: 'impactMarket web',
                 url: 'https://impactmarket.com'
             }}
-            network={Alfajores}
+            network={network}
         >
             {typeof window === 'undefined' ? null : <Wrapper>{children}</Wrapper>}
         </ContractKitProvider>
