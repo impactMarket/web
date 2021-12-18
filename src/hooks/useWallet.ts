d97667af5ef07aec4730eeac50f68eca779c38f8
Paulo Soares <psoares.bj@gmail.com>
2021-12-18 16:02:51 +0000

    Add token page redirect to wallet connect

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -1,17 +1,31 @@
 import { useContractKit } from '@celo-tools/use-contractkit';
+import { useRouter } from 'next/router';
 
 export const useWallet = () => {
     const { address, connect: connectFromHook, destroy } = useContractKit();
+    const { asPath, push } = useRouter();
 
     const connect = async () => {
         try {
             await connectFromHook();
+
+            if (asPath !== '/governance-token') {
+                return push('/governance-token');
+            }
         } catch (error) {
             console.log(error);
         }
     };
 
-    const disconnect = async () => await destroy().catch(error => console.log(error));
+    const disconnect = async () => {
+        try {
+            await destroy();
+
+            return;
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return { address, connect, disconnect };
 };
