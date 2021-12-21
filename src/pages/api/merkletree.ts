fd8909a149cbafd5a8d6ccecda8b064ef6c8c616
Paulo Soares <psoares.bj@gmail.com>
2021-12-21 17:57:58 +0000

    Fix types

--- src/pages/api/merkletree.ts
+++ src/pages/api/merkletree.ts
@@ -1,26 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
 const fetch = require('node-fetch');
 const testnetMerkleTree = require('../../../_files/testnet-merkletree.json');
 
-const url = 'https://impactmarket-backend-airgrab.herokuapp.com/';
+const url = (address: string | string[]) => `https://impactmarket-backend-airgrab.herokuapp.com/${address}`;
 
-const merkleTreeHandler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+const merkleTreeHandler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
     try {
-        const { testnet } = req?.query || {};
+        const { address, testnet } = req?.query || {};
 
         if (!!testnet) {
             res.status(200).json({ merkleTree: testnetMerkleTree });
         } else {
-            const response = await fetch(url);
+            const response = await fetch(url(address));
             const file = await response.json();
 
             console.log(response);
 
-            res.status(200).json({ merkleTree: file }).end();
+            res.status(200).json({ merkleTree: file });
         }
     } catch (error) {
         console.log(error);
 
-        res.status(500).end();
+        res.status(500);
     }
 };
 
