688dc056fe0b2d1f3c05ec5a59825493570cbf73
Paulo Soares <psoares.bj@gmail.com>
2022-01-12 19:28:52 +0000

    Fix description line-break issue

--- src/theme/variables/generator.ts
+++ src/theme/variables/generator.ts
@@ -24,7 +24,8 @@ export const generator = {
         { list: fonts.families, cssProp: 'font-family' },
         { list: fonts.weights, cssProp: 'font-weight' },
         { list: { relative: 'relative' }, cssProp: 'position' },
-        { list: { underlined: 'underline' }, cssProp: 'text-decoration' }
+        { list: { underlined: 'underline' }, cssProp: 'text-decoration' },
+        { list: { preline: 'pre-line' }, cssProp: 'white-space' }
     ],
 
     // Space props
