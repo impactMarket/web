74b26c171c9e871b3ec967ca60981d3f28ffa641
Paulo Soares <psoares.bj@gmail.com>
2022-01-15 02:05:48 +0000

    Fix Ubeswap pricing widget

--- src/page-components/Governance/Governance.tsx
+++ src/page-components/Governance/Governance.tsx
@@ -1,14 +1,14 @@
 import { GovernanceArticles } from '../../components/GovernanceArticles/GovernanceArticles';
 import { Hero } from './Hero/Hero';
 import { Illustration } from './Illustration/Illustration';
-// import { Tokenomics } from './Tokenomics/Tokenomics';
+import { Tokenomics } from './Tokenomics/Tokenomics';
 import React from 'react';
 
 export const Governance = () => {
     return (
         <>
             <Hero />
-            {/* <Tokenomics /> */}
+            <Tokenomics />
             <GovernanceArticles sBackground="backgroundSecondaryDisabled" sPadding={{ md: '4 0', xs: '2 0' }} />
             <Illustration />
         </>
