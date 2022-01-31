5bd7eb46cf1a45b99d2715c6f28e1ffdf476b0f4
Paulo Soares <psoares.bj@gmail.com>
2022-01-31 16:09:52 +0000

    Update breakdown

--- src/page-components/Governance/Hero/Hero.tsx
+++ src/page-components/Governance/Hero/Hero.tsx
@@ -9,36 +9,15 @@ import {
     RichContentFormat,
     Row,
     Section,
-    Text,
-    TextLink
+    Text
 } from '../../../theme/components';
-import { currencyValue } from '../../../helpers/currencyValue';
 import { modal } from 'react-modal-handler';
-import { useEpoch } from '@impact-market/utils';
-import { useTranslation } from '../../../components/TranslationProvider/TranslationProvider';
-import { useWallet } from '../../../hooks/useWallet';
-import React, { useEffect, useState } from 'react';
-
-const exampleUsdValue = 100;
-
-const getExampleClaimValue = (epoch: { rewards?: number; totalRaised?: number } | undefined) =>
-    epoch ? (exampleUsdValue * epoch?.rewards) / (exampleUsdValue + epoch?.totalRaised) : 0;
+import React from 'react';
 
 export const Hero = () => {
-    const { address } = useWallet();
-    const { epoch } = useEpoch();
-    const [exampleClaimValue, setExampleClaimValue] = useState(getExampleClaimValue(epoch));
-    const { t } = useTranslation();
-
     const handleContributeClick = () => {
         return modal.open('governanceContribute');
     };
-
-    useEffect(() => {
-        const exampleClaimValue = (exampleUsdValue * epoch?.rewards) / (exampleUsdValue + epoch?.totalRaised);
-
-        setExampleClaimValue(exampleClaimValue);
-    }, [epoch]);
 
     return (
         <Section relative>
@@ -50,28 +29,7 @@ export const Hero = () => {
                             <String id="page.governanceToken.hero.heading" />
                         </Heading>
                         <RichContentFormat fontSize={{ md: '16 28', xs: '14 24' }} mt={1}>
-                            <String
-                                id="page.governanceToken.hero.text"
-                                variables={{
-                                    // eslint-disable-next-line no-nested-ternary
-                                    convertedValue: exampleClaimValue
-                                        ? `~${currencyValue(exampleClaimValue, { isToken: true, symbol: 'PACT' })}`
-                                        : address
-                                        ? '*'
-                                        : `(${t('connectToWallet')})`,
-                                    value: currencyValue(exampleUsdValue, { isToken: true, symbol: 'cUSD' })
-                                }}
-                            />
-                            <Div mt={1}>
-                                <TextLink
-                                    brandPrimary
-                                    href="http://docs.impactmarket.com/"
-                                    rel="noreferrer noopener"
-                                    target="_blank"
-                                >
-                                    <String id="page.governanceToken.hero.learnMoreLabel" />
-                                </TextLink>
-                            </Div>
+                            <String id="page.governanceToken.hero.text" />
                         </RichContentFormat>
                         <Div mt={2} sAlignItems="center" sFlexDirection={{ sm: 'row', xs: 'column' }}>
                             <Button
