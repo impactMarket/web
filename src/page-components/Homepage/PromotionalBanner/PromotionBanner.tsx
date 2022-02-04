191e5fbe0e3fc424984d8ad3a81c9768b17cf344
Paulo Soares <psoares.bj@gmail.com>
2022-02-04 10:21:01 +0000

    Refactor promotional banner

---
+++ src/page-components/Homepage/PromotionalBanner/PromotionBanner.tsx
@@ -0,0 +1,36 @@
+import { PrismicRichTextType } from '../../../lib/Prismic/types';
+import { colors } from '../../../theme';
+import { usePrismicData } from '../../../lib/Prismic/components/PrismicDataProvider';
+import React from 'react';
+import RichText from '../../../lib/Prismic/components/RichText';
+import styled from 'styled-components';
+
+type PromotionalBannerType = {
+    content?: PrismicRichTextType;
+    isActive?: boolean;
+};
+
+const PromotionalBannerWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 2rem;
+    background-color: ${colors.backgroundSecondary};
+    padding: 0.625rem 2rem;
+`;
+
+const PromotionalBanner = () => {
+    const { extractFromPage } = usePrismicData();
+    const { content, isActive } = extractFromPage('promotionalBanner') as PromotionalBannerType;
+
+    if (!isActive) {
+        return null;
+    }
+
+    return (
+        <PromotionalBannerWrapper>
+            <RichText bold center content={content} />
+        </PromotionalBannerWrapper>
+    );
+};
+
+export default PromotionalBanner;
