4dc833d6cac289f375883655a3bcbef189c9633b
Paulo Soares <psoares.bj@gmail.com>
2022-02-22 16:36:14 +0000

    swap dashboard to SSR

--- src/pages/global-dashboard.tsx
+++ src/pages/global-dashboard.tsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line import/named
 import { ClientConfig } from '@prismicio/client';
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import { GlobalDashboard } from '../page-components';
 import Api from '../apis/api';
 import Prismic from '../lib/Prismic/Prismic';
 import getTypesToFetchWithConfigs from '../lib/Prismic/helpers/getTypesToFetchWithConfigs';
 
-export const getStaticProps: GetStaticProps = async ({ locale: lang, previewData }) => {
+export const getServerSideProps: GetServerSideProps = async ({ locale: lang, previewData }) => {
     const clientOptions = previewData as ClientConfig;
 
     const types = getTypesToFetchWithConfigs(['website_global_dashboard_page']);
