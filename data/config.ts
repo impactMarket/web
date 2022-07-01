729f0b7b33a240f4b10461f3bd5bd14dc7fb01af
Paulo Soares <psoares.bj@gmail.com>
2022-07-01 18:06:29 +0100

    Update dao articles

--- data/config.ts
+++ data/config.ts
@@ -3,16 +3,6 @@ export const config = {
     cta: {
         image: '/img/app-mockup.png'
     },
-    governanceArticles: [
-        {
-            icon: 'im',
-            url: 'https://app.ubeswap.org/#/swap?outputCurrency=0x46c9757c5497c5b1f2eb73ae79b6b67d119b0b58',
-            urlLabelKey: 'buyPactOnUbeswap'
-        },
-        { image: '/img/impact-market-dao.svg' },
-        { icon: 'vote' },
-        { icon: 'megaphone' }
-    ],
     tokenMetrics: [
         { name: 'priceCUSD' },
         { name: 'circulatingSupply' },
--- src/page-components/Governance/Governance.tsx
+++ src/page-components/Governance/Governance.tsx
@@ -2,14 +2,23 @@ import { GovernanceArticles } from '../../components/GovernanceArticles/Governan
 import { Hero } from './Hero/Hero';
 import { Illustration } from './Illustration/Illustration';
 import { Tokenomics } from './Tokenomics/Tokenomics';
+import { usePrismicData } from '../../lib/Prismic/components/PrismicDataProvider';
 import React from 'react';
 
 export const Governance = () => {
+    const { data } = usePrismicData();
+    const { daoArticles } = data || {};
+    const articles = daoArticles?.data?.articles;
+
     return (
         <>
             <Hero />
             <Tokenomics />
-            <GovernanceArticles sBackground="backgroundSecondaryDisabled" sPadding={{ md: '4 0', xs: '2 0' }} />
+            <GovernanceArticles
+                articles={articles as any}
+                sBackground="backgroundSecondaryDisabled"
+                sPadding={{ md: '4 0', xs: '2 0' }}
+            />
             <Illustration />
         </>
     );
