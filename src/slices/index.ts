cba3fd1c30f1109e0720857c6d155be6041aec03
Paulo Sousa <paulo.86.sousa@gmail.com>
2022-08-12 16:37:16 +0100

    Adress comments from review

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -5,10 +5,8 @@ const slices = {
     Hero: dynamic(() => import('./Hero'), { ssr: false }),
     DocumentLink: dynamic(() => import('./DocumentLink'), { ssr: false }),
     LogoList: dynamic(() => import('./LogoList'), { ssr: false }),
-    MobileAppCta: dynamic(() => import('./MobileAppCta'), { ssr: false }),
     NumbersFromApi: dynamic(() => import('./NumbersFromApi'), { ssr: false }),
     SubscribeCta: dynamic(() => import('./SubscribeCta'), { ssr: false }),
-    VideoSection: dynamic(() => import('./VideoSection'), { ssr: false })
 } as { [componentName: string]: ComponentType | Function };
 
 export default slices;
--- src/theme/components/Button/Button.tsx
+++ src/theme/components/Button/Button.tsx
@@ -70,6 +70,26 @@ const colorVariations = {
         }
     `,
 
+    linedSecondaryDark: css`
+        background-color: transparent;
+        border-radius: 0.75rem;
+        border: 0.0625rem solid ${colors.backgroundSecondary};
+        color: ${colors.g700};
+
+        > p {
+            font-weight: 500;
+        }
+
+        &:not(:disabled) {
+            &:hover {
+                background-color: ${colors.brandPrimary};
+                border-color: transparent;
+                box-shadow: 0 0 16px rgba(0, 0, 0, 0.16);
+                color: ${colors.white};
+            }
+        }
+    `,
+
     pagination: css`
         background-color: ${colors.backgroundSecondary};
         color: ${colors.textPrimary};
@@ -159,6 +179,12 @@ const sizeVariations = {
         min-height: 44px;
         padding: 4px 16px;
         min-width: 44px;
+    `,
+
+    rebranded: css`
+        font-size: 16px;
+        min-height: 48px;
+        padding: 12px 20px;
     `,
 
     small: css`
