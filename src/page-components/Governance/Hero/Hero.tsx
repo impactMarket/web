8921a03805ec1e86a2e637f57fc805cb829494a0
Paulo Sousa <paulo.86.sousa@gmail.com>
2022-10-11 17:35:54 +0100

    Add ramp sdk and cashin button, update package.json

--- src/page-components/Governance/Hero/Hero.tsx
+++ src/page-components/Governance/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import { Breakdown, ShareThis, String } from '../../../components';
+import { colors } from '../../../theme';
 import {
     Button,
     Col,
@@ -12,11 +13,26 @@ import {
     Text
 } from '../../../theme/components';
 import { modal } from 'react-modal-handler';
+import { RampInstantSDK } from "@ramp-network/ramp-instant-sdk";
+import { useTranslation } from '../../../components/TranslationProvider/TranslationProvider';
+import config from '../../../../config';
 import React from 'react';
 
 export const Hero = () => {
+    const { t } = useTranslation();
+
     const handleContributeClick = () => {
         return modal.open('governanceContribute');
+    };
+
+    const triggerRamp = () => {
+        new RampInstantSDK({
+            hostAppName: 'impactMarket',
+            hostLogoUrl: 'https://www.gitbook.com/cdn-cgi/image/height=40,fit=contain,dpr=2,format=auto/https%3A%2F%2F2074548608-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FCYFjCZsJmtFWjyXZmVH8%252Flogo%252FrnP1xUXSL3jJHhxKkA82%252Flogo_blue_on_transparent_background.png%3Falt%3Dmedia%26token%3D42e89872-c292-4ccc-b8c5-771b055c4581',
+            swapAmount: '',
+            swapAsset: "CELO_CUSD",
+            userAddress: config.treasuryAddress
+        }).show();
     };
 
     return (
@@ -43,6 +59,26 @@ export const Hero = () => {
                                 </Text>
                             </Button>
                         </Div>
+                        <Text flex g500 mt={0.5} small>
+                            {t('or').toLocaleLowerCase()}
+                        </Text>
+                        <Button
+                            lined
+                            mt={{ xs: .5 }}
+                            onClick={triggerRamp}
+                            sColor={colors.brandPrimary}
+                            sHeight="3rem"
+                            sPadding="12px 20px"
+                            smaller
+                        >
+                            <Text
+                                bold
+                                sFontSize="1rem"
+                                sFontWeight={500}
+                            >
+                                <String id="cashin.button.text" />
+                            </Text>
+                        </Button>
                         <ShareThis mt={2} sJustifyContent={{ sm: 'left', xs: 'center' }} />
                     </Col>
                     <Col md={6} mt={{ md: 0, xs: 2.5 }} relative xs={12}>
