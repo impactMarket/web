eb9de0c59ae45a02fe48927c1bb20e45aa079967
Hugo Marques <62065869+hugomarquesdev@users.noreply.github.com>
2023-01-17 14:17:07 +0000

    Learn And Earn page (#280)
    Co-authored-by: Hugo Marques <hugomarquesdev@MacBook-Pro-de-Hugo.local>

---
+++ src/page-components/LearnAndEarn.tsx
@@ -0,0 +1,33 @@
+import { usePrismicData } from '../lib/Prismic/components/PrismicDataProvider';
+import React, { useEffect, useState } from 'react';
+import Slices from '../lib/Prismic/components/Slices';
+
+import { CommunityMetricsProvider } from '../components/CommunityMetricsProvider/CommunityMetricsProvider';
+import Api from '../apis/api';
+
+export const LearnAndEarn = () => {
+    const { page } = usePrismicData();
+    const slices = page?.data?.body;
+
+    const [numbers, setNumbers] = useState<any>();
+
+    useEffect(() => {
+        const getNumbers = async () => {
+            try {
+                const numbers = (await Api.getGlobalNumbers()) as any;
+
+                setNumbers(numbers);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        getNumbers();
+    }, []);
+
+    return (
+        <CommunityMetricsProvider metrics={numbers}>
+            <Slices slices={slices} />
+        </CommunityMetricsProvider>
+    );
+};
